704c524eec450f1f929707d291fffac48b7496b6
Camille MAPOULA <cammapou@e2r9p13.42.fr>
2018-06-14 11:43:59 +0200

    algo_min 5 nbr

---
+++ srcs_pushswap/algo_min.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algo_min.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cammapou <cammapou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/14 10:57:30 by cammapou          #+#    #+#             */
+/*   Updated: 2018/06/14 11:34:31 by cammapou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void 		ft_suite_pa(t_ev *ev, t_list **lst_a, t_list **lst_b)
+{
+	ev->len_b = ft_stack_len_b(*lst_b);
+	while (ev->len_b)
+	{
+		ft_pa(lst_a, lst_b);
+		  ft_putendl("pa");
+		ev->len_b--;
+	}
+}
+
+void ft_quick_min(t_ev *ev, t_list **lst_a, t_list **lst_b)
+{
+    while (1)
+    {
+        while (ft_check_val(*lst_a, ev->med) == -1)
+        {
+            ev->len_a = ft_stack_len_a(*lst_a);
+            if (ev->len_a == 3)
+                break ;
+            if (*(int*)(*lst_a)->content < ev->med)
+            {
+                ft_pb(lst_a, lst_b);
+                ft_printf("pb\n");
+            }
+            else
+            {
+                ft_ra(lst_a);
+                ft_printf("ra\n");
+            }
+        }
+        if (ev->len_a == 3)
+        {
+            ft_algomin(lst_a);
+            break ;
+        }
+        ev->med++;
+    }
+    ft_suite_pa(ev, lst_a, lst_b);
+}
